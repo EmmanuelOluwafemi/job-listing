586ebb5b5c21e8b8d1a7631d2491e12520ba5d63
EmmanuelOluwafemi <46839679+EmmanuelOluwafemi@users.noreply.github.com>
2020-09-18 08:59:07 +0100

    Updates

---
+++ precache-manifest.a525252f662550e7495f1b65d5a87903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a5bf000c887cec6e9b760467a1afd9",
+    "url": "/job-listing/index.html"
+  },
+  {
+    "revision": "3d175ade5acbd984fec8",
+    "url": "/job-listing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9bf7f2b0ce3bb39e9e56",
+    "url": "/job-listing/static/js/2.10a49efb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-listing/static/js/2.10a49efb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d175ade5acbd984fec8",
+    "url": "/job-listing/static/js/main.79879ef3.chunk.js"
+  },
+  {
+    "revision": "cb84270f42c17b7bdfb8",
+    "url": "/job-listing/static/js/runtime-main.3f5338fb.js"
+  }
+]);
